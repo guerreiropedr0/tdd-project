47dc4ee570392006cdba42815c3a717a42eed95f
Pedro Guerreiro <pmoreiraguerreiro@gmail.com>
2022-04-14 09:44:43 +0100

    Continue TDD process for Solver class

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -3,11 +3,11 @@ require_relative '../solver'
 describe Solver do
   context 'When testing the Solver class' do
     before :each do
-      solver = Solver.new
+      @solver = Solver.new
     end
 
     it 'should have an object with the instance of Solver' do
-      solver_class = solver.class
+      solver_class = @solver.class
 
       expect(solver_class).to be Solver
     end
