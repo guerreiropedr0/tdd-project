47c65f4c336849e0645010ccf84d2fab93de4081
Pedro Guerreiro <pmoreiraguerreiro@gmail.com>
2022-04-14 09:48:00 +0100

    Start TDD process for factorial method

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -11,5 +11,11 @@ describe Solver do
 
       expect(solver_class).to be Solver
     end
+
+    it 'should return the factorial when calling factorial method' do
+      number = @solver.factorial(5)
+
+      expect(number).to eq 120
+    end
   end
 end
