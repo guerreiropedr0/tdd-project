a3c82225a31be1e241479f6c6526414ed08010b7
Pedro Guerreiro <pmoreiraguerreiro@gmail.com>
2022-04-14 09:43:14 +0100

    Start TDD process for Solver class

---
+++ spec/solver_spec.rb
@@ -0,0 +1,15 @@
+require_relative '../solver'
+
+describe Solver do
+  context 'When testing the Solver class' do
+    before :each do
+      solver = Solver.new
+    end
+
+    it 'should have an object with the instance of Solver' do
+      solver_class = solver.class
+
+      expect(solver_class).to be Solver
+    end
+  end
+end
