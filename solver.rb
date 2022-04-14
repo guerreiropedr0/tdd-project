926083a41cec643c317b0b87e1395ad02434abcd
Pedro Guerreiro <pmoreiraguerreiro@gmail.com>
2022-04-14 09:53:16 +0100

    Finish TDD process for factorial method

--- solver.rb
+++ solver.rb
@@ -1,5 +1,9 @@
 class Solver
-  def factorial
+  def factorial(integer)
+    factorial_number = 1
+    return 1 if n.zero?
 
+    integer.times { |num| factorial_number *= num + 1 }
+    factorial_number
   end
 end
